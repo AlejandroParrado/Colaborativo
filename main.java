1d3934bc0217391d62ebe2a97b65dbb39ad29702
AlejandroParrado <alejandroparrado04@gmail.com>
2024-12-15 15:23:04 +0100

    a

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        holaalejandro holaalejandro = new holaalejandro();
+        holaalvaro holaalvaro = new holaalvaro();
+        holadani holadani = new holadani();
+    }
+}
